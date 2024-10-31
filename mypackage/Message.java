73a9b6a564d0dd17301c8f00c2bfc9785fab6235
Amil Mether <amilmether37@gmail.com>
2024-10-31 20:17:02 +0530

    demo

---
+++ mypackage/Message.java
@@ -0,0 +1,15 @@
+// Save this file as Message.java inside a folder named "mypackage"
+package mypackage;
+
+public class Message {
+    
+    public void display() {
+        System.out.println("Hello from the mypackage package!");
+    }
+
+    void hecker(){
+
+
+        System.out.println("I am Not an Hacker");
+    }
+}
