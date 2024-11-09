e1a35fdd0cba08f0d669a03ea06854b0f2d0221e
Amil Mether <amilmether37@gmail.com>
2024-11-09 19:37:51 +0530

    Palindrome of String Completed

--- LAB_EXAM/PalindromOfString.java
+++ LAB_EXAM/PalindromOfString.java
@@ -1,5 +1,18 @@
-package LAB_EXAM;
-
 public class PalindromOfString {
-    
+    public static void main(String[] args){
+        int len=args[0].length();
+        int flag=0;
+        System.out.println(len);
+        for(int i=0;i<=len/2;i++){
+            if(args[0].charAt(i)!=args[0].charAt(len-1-i)){
+                flag=1;
+                break;
+            }
+        }
+        if (flag==0) {
+            System.out.println("The Given String is Palindrome");
+        }else{
+            System.out.println("The given String is not a palindrome");
+        }
+    }    
 }
